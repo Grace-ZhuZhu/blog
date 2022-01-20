62a0d476f6892b8e2d04973f5a36cd25d58b7824
Shu Zhu <zs.grace@outlook.com>
2022-01-20 15:55:42 +0800

    Add debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,9 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
+    logger.debug "New article: #{@article.attributes.inspect}"
+    logger.debug "Article should be valid: #{@article.valid?}"
+
     if @article.save
       redirect_to @article
     else
